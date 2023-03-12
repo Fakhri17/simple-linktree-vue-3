86741f574447f7411932b475d75fde9965c870e7
Fakhri17 <fakhricoffe@gmail.com>
2023-03-12 18:44:04 +0700

    add: effect text decoration

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,12 @@
   <div class="bg-light py-3 px-4 w-100 rounded-3 shadow mb-5 ">
     <div class="row align-items-center">
       <div class="col">
-        <router-link to="/" class="text-decoration-none fw-bold text-dark fs-2">Home</router-link>
+        <router-link to="/" class="text-link-header fw-bold text-dark fs-2">Home</router-link>
       </div>
       <div class="col-auto">
-        <router-link v-if="getUsername().length" to="/dashboard" class="fw-bold btn-link text-dark fs-5 me-3">Dashboard</router-link>
-        <router-link v-if="!getUsername().length" to="/login" class="fw-bold btn-link text-dark fs-5">Login</router-link>
-        <router-link v-else @click="resetState()" to="/" class="fw-bold btn-link text-dark fs-5">Logout</router-link>
+        <router-link v-if="getUsername().length" to="/dashboard" class="fw-bold text-link-header text-dark fs-5 me-3">Dashboard</router-link>
+        <router-link v-if="!getUsername().length" to="/login" class="fw-bold text-link-header text-dark fs-5">Login</router-link>
+        <router-link v-else @click="resetState()" to="/" class="fw-bold text-link-header text-dark fs-5">Logout</router-link>
       </div>
     </div>
   </div>
